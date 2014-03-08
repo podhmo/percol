3989f0a2d32ef248b44898d0f8b98c50154e84fb
podhmo <podhmo+altair@beproud.jp>
2014-03-09 01:33:51 +0900

    remove compat functions not used

--- percol/compat.py
+++ percol/compat.py
@@ -1,27 +1,8 @@
 # -*- coding:utf-8 -*-
-import inspect
-import platform
 import sys
 import types
 
 ## copy and paste from pyramid(https://raw.github.com/Pylons/pyramid/master/pyramid/compat.py)
-
-if platform.system() == 'Windows': # pragma: no cover
-    WIN = True
-else: # pragma: no cover
-    WIN = False
-
-try: # pragma: no cover
-    import __pypy__
-    PYPY = True
-except: # pragma: no cover
-    __pypy__ = None
-    PYPY = False
-
-try:
-    import cPickle as pickle
-except ImportError: # pragma: no cover
-    import pickle
 
 # True if we are running on Python 3.
 PY3 = sys.version_info[0] == 3
@@ -99,30 +80,6 @@ Python 2: If ``s`` is an instance of ``text_type``, return
 """
 
 if PY3: # pragma: no cover
-    from urllib import parse
-    urlparse = parse
-    from urllib.parse import quote as url_quote
-    from urllib.parse import quote_plus as url_quote_plus
-    from urllib.parse import unquote as url_unquote
-    from urllib.parse import urlencode as url_encode
-    from urllib.request import urlopen as url_open
-    url_unquote_text = url_unquote
-    url_unquote_native = url_unquote
-else:
-    import urlparse
-    from urllib import quote as url_quote
-    from urllib import quote_plus as url_quote_plus
-    from urllib import unquote as url_unquote
-    from urllib import urlencode as url_encode
-    from urllib2 import urlopen as url_open
-    def url_unquote_text(v, encoding='utf-8', errors='replace'): # pragma: no cover
-        v = url_unquote(v)
-        return v.decode(encoding, errors)
-    def url_unquote_native(v, encoding='utf-8', errors='replace'): # pragma: no cover
-        return native_(url_unquote_text(v, encoding, errors))
-        
-
-if PY3: # pragma: no cover
     import builtins
     exec_ = getattr(builtins, "exec")
 
@@ -185,7 +142,6 @@ else:
     imap_ = itertools.imap
 
 
-    
 if PY3: # pragma: no cover
     def is_nonstr_iter(v):
         if isinstance(v, str):
@@ -194,64 +150,3 @@ if PY3: # pragma: no cover
 else:
     def is_nonstr_iter(v):
         return hasattr(v, '__iter__')
-    
-if PY3: # pragma: no cover
-    im_func = '__func__'
-    im_self = '__self__'
-else:
-    im_func = 'im_func'
-    im_self = 'im_self'
-
-try: # pragma: no cover
-    import configparser
-except ImportError: # pragma: no cover
-    import ConfigParser as configparser
-
-try:
-    from Cookie import SimpleCookie
-except ImportError: # pragma: no cover
-    from http.cookies import SimpleCookie
-
-if PY3: # pragma: no cover
-    from html import escape
-else:
-    from cgi import escape
-
-try: # pragma: no cover
-    input_ = raw_input
-except NameError: # pragma: no cover
-    input_ = input
-
-
-try: 
-    from StringIO import StringIO as NativeIO
-except ImportError: # pragma: no cover
-    from io import StringIO as NativeIO
-
-# "json" is not an API; it's here to support older pyramid_debugtoolbar
-# versions which attempt to import it
-import json
-
-    
-if PY3: # pragma: no cover
-    # see PEP 3333 for why we encode WSGI PATH_INFO to latin-1 before
-    # decoding it to utf-8
-    def decode_path_info(path):
-        return path.encode('latin-1').decode('utf-8')
-else:
-    def decode_path_info(path):
-        return path.decode('utf-8')
-
-if PY3: # pragma: no cover
-    # see PEP 3333 for why we decode the path to latin-1 
-    from urllib.parse import unquote_to_bytes
-    def unquote_bytes_to_wsgi(bytestring):
-        return unquote_to_bytes(bytestring).decode('latin-1')
-else:
-    from urlparse import unquote as unquote_to_bytes
-    def unquote_bytes_to_wsgi(bytestring):
-        return unquote_to_bytes(bytestring)
-
-def is_bound_method(ob):
-    return inspect.ismethod(ob) and getattr(ob, im_self, None) is not None
-
