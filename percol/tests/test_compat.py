ad5eb4dee00d765d6bd23daa721d2e1719147044
podhmo <podhmo+altair@beproud.jp>
2014-03-08 23:59:07 +0900

    copy compat library from pyramid

---
+++ percol/tests/test_compat.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+import unittest
+
+class DontReach(Exception):
+    pass
+
+class DummyIterator(object):
+    def __iter__(self):
+        yield 1
+        raise DontReach()
+
+class LazynessIterationTests(unittest.TestCase):
+    def test_iteritems__map(self):
+        from percol.compat import (
+            iteritems_,
+            map_
+        )
+
+        dummy = iter(DummyIterator())
+        target = {1: dummy, 2:dummy}
+
+        with self.assertRaises(DontReach):
+            next(map_(lambda (k, it): next(it), iteritems_(target)))
+
+    def test_iteritems__imap(self):
+        from percol.compat import (
+            iteritems_,
+            imap_
+        )
+
+        dummy = iter(DummyIterator())
+        target = {1: dummy, 2:dummy}
+
+        result = next(imap_(lambda (k, it): next(it), iteritems_(target)))
+        self.assertEqual(result, 1)
+
